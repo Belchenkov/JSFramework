92af25946c7c40e13270605eeb72dff2d1d087f0
Belchenkov <u608110@gmail.com>
2017-09-02 18:25:50 +0300

    Bind Components, Header & Preloader

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,8 +9,24 @@ class AppComponent extends WFMComponent {
 export const appComponent = new AppComponent({
     selector: 'app-root',
     template: `
-        <div>
-            <h4>App component works!</h4>
+        <app-header></app-header>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col z-depth-3 s8 center-align offset-s2" style="margin-top: 30px;">
+                  <div class="card grey darken-1">
+                    <div class="card-content white-text">
+                      <span class="card-title ">Welcome</span>
+                      <p>I am a very simple card. I am good at containing small bits of information.
+                      I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                      <a href="#" class="waves-effect waves-light btn">Next Page</a>
+                    </div>
+                </div>
+            </div>
+      </div>
+            
         </div>
     `
 });
